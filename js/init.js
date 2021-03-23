666a586c0e6538bc43cb3020e6291646bc46f63b
Form23 <81198478+Form23@users.noreply.github.com>
2021-03-22 18:08:34 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,12 @@
-
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA4iQCHklcaIPAC1GwfnZT8bhVelNUmupU",
+    authDomain: "autentifica43ador-75427.firebaseapp.com",
+    projectId: "autentifica43ador-75427",
+    storageBucket: "autentifica43ador-75427.appspot.com",
+    messagingSenderId: "663290664377",
+    appId: "1:663290664377:web:d20a0f035f0f3066901268",
+    measurementId: "G-YYWS5N8S5J"
+    });
